0b08e5cd412e9bfd08c139c56000b2692701b703
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-11-19 12:35:52 +0200

    task-4

--- src/components/GaleryList/GaleryList.jsx
+++ src/components/GaleryList/GaleryList.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Grid, GridItem, CardItem } from 'components';
 
-export const GaleryList = ({ photos }) => {
+export const GaleryList = ({ photos, handleUrl }) => {
   return (
     <Grid>
       {photos.map(item => (
         <GridItem key={item.id}>
           <CardItem color={item.avg_color}>
-            <img src={item.src.large} alt={item.alt} />
+            <img
+              src={item.src.small}
+              alt={item.alt}
+              onClick={() => {
+                handleUrl(item.src.large);
+              }}
+            />
           </CardItem>
         </GridItem>
       ))}
